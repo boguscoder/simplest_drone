5e8d92be50fa78a8d321c75d702a9e69b4847052
Oleg Bogdanov <boguscoder@gmail.com>
2025-06-18 23:11:09 -0700

    simplify usb handling and add  handler to usb-logger

--- drone.sh
+++ drone.sh
@@ -4,4 +4,4 @@ cargo run --release --features $log_type
 cd ~/git/drone_plotter
 cargo build --release --features $log_type
 sleep 5
-socat file:/dev/tty.usbmodem0xC0DECAFE1,ispeed=115200,ospeed=115200,raw,echo=0,nonblock,waitlock=/tmp/usbmodem.lock - | ~/git/drone_plotter/target/release/stream_plotter
+socat file:/dev/tty.usbmodem83101,ispeed=115200,ospeed=115200,raw,echo=0,nonblock,waitlock=/tmp/usbmodem.lock - | ~/git/drone_plotter/target/release/stream_plotter
